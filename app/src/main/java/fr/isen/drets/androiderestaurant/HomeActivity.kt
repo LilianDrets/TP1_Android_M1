35f0968bfc34af7c4678c99158bf68bccc46cf40
LilianDrets <ldrets@free.fr>
2023-02-28 18:47:22 +0100

    Premier commit

---
+++ app/src/main/java/fr/isen/drets/androiderestaurant/HomeActivity.kt
@@ -0,0 +1,39 @@
+package fr.isen.drets.androiderestaurant
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+
+class HomeActivity : AppCompatActivity() {
+
+    fun createListeActivity(Titre : String, Liste : String) {
+        val listeActivity = Intent(this, ListeActivity::class.java)
+        listeActivity.putExtra("Titre", Titre)
+        listeActivity.putExtra("Liste", Liste)
+        startActivity(listeActivity)
+        finish()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        val textEntrees = findViewById<TextView>(R.id.textEntrees)
+        val textPlats = findViewById<TextView>(R.id.textPlats)
+        val textDesserts = findViewById<TextView>(R.id.textDesserts)
+
+        textEntrees.setOnClickListener {
+            createListeActivity("Entrées", "ListeEntrées")
+        }
+
+        textPlats.setOnClickListener {
+            createListeActivity("Plats", "ListePlats")
+        }
+
+        textDesserts.setOnClickListener {
+            createListeActivity("Desserts", "ListeDesserts")
+        }
+    }
+}
---
+++ app/src/main/java/fr/isen/drets/androiderestaurant/ListeActivity.kt
@@ -0,0 +1,22 @@
+package fr.isen.drets.androiderestaurant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class ListeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_liste)
+
+        /*val Titre = intent.getStringExtra("Titre")
+        val Liste = intent.getStringExtra("Liste")
+
+        val titreListe = findViewById<TextView>(R.id.TitreListe)
+        val liste = findViewById<TextView>(R.id.Liste)
+
+        titreListe.setText(Titre)
+        liste.setText(Liste)*/
+
+    }
+}
